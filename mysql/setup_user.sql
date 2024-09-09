c92915d45e55d0dda26b391ca95f7546640cc425
oladapsy <dapoodedeyi@yahoo.com>
2024-09-09 17:54:23 +0000

    sql setup

--- mysql/setup_user.sql
+++ mysql/setup_user.sql
@@ -1,14 +1,11 @@
 -- Create a Mysql Server with:
--- Databse co2db
+-- Database co2db
 -- User oladapsy with password 1234 in localhost
--- Grand all privileges for oladapsy on co2db
+-- Grant all privileges for oladapsy on co2db
 
 CREATE DATABASE IF NOT EXISTS co2db;
-CREATE USER
-    IF NOT EXIST 'oladapsy'@'localhost'
-    IDENTIFIED BY '1234';
+CREATE USER IF NOT EXISTS 'oladapsy'@'localhost' IDENTIFIED BY '1234dapo';
 GRANT ALL PRIVILEGES
-    ON 'co2db'.*
-    TO 'oladapsy'@'localhost'
-    IDENTIFIED BY '1234';
+    ON co2db.*
+    TO 'oladapsy'@'localhost';
 FLUSH PRIVILEGES;
