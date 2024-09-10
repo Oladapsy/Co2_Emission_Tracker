f14025b1e9f9b7c070ea42592a8e48659a5d0861
oladapsy <dapoodedeyi@yahoo.com>
2024-09-10 09:00:21 +0000

    setup update

--- mysql/setup_user.sql
+++ mysql/setup_user.sql
@@ -4,7 +4,7 @@
 -- Grant all privileges for oladapsy on co2db
 
 CREATE DATABASE IF NOT EXISTS co2db;
-CREATE USER IF NOT EXISTS 'oladapsy'@'localhost' IDENTIFIED BY '1234dapo';
+CREATE USER IF NOT EXISTS 'oladapsy'@'localhost' IDENTIFIED BY '1234';
 GRANT ALL PRIVILEGES
     ON co2db.*
     TO 'oladapsy'@'localhost';
